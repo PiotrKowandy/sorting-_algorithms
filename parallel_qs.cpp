d052207d7f7a20ad5ab069c4fed1b82f62cf9eba
Piotr Kowańdy <33897068+PiotrKowandy@users.noreply.github.com>
2020-09-15 18:27:34 +0200

    Update parallel_qs.cpp
    std::async used

--- parallel_qs.cpp
+++ parallel_qs.cpp
@@ -2,17 +2,11 @@
 #include <vector>
 #include <thread>
 #include <utility>
-#include <mutex>
 #include <chrono>
-#include <atomic>
-
-std::vector<std::thread> bunch_of_threads;
-std::mutex mtx;
-int number_of_threads = std::thread::hardware_concurrency();
-std::atomic<int> actual_threads(0);
+#include <future>
 
 void print_tab(int* tab, int n){
-        for (int i=0; i< n; i++){
+    for (int i=0; i< n; i++){
         std::cout<< tab[i] << ", ";
     }
     std::cout<<"\n";
@@ -23,9 +17,8 @@ void quick_sort(int* tab, int l, int r){
     if (l >= r){   
         return;
     }
-    std::lock_guard<std::mutex> lg(mtx);
 
-    std::atomic<int> i(l - 1);
+    int i = l - 1;
     int pivot = tab[r];
 
     for (int j = l; j <= r-1; j++){
@@ -38,26 +31,15 @@ void quick_sort(int* tab, int l, int r){
     int p = i+1;
     std::swap(tab[i+1], tab[r]);
     
-    if (actual_threads <= number_of_threads){
-        actual_threads++;
-        bunch_of_threads.emplace_back(std::thread(quick_sort, tab, l, p-1));
-    }
-    else{
-        quick_sort(tab, l, p-1);
-    }
-    if (actual_threads <= number_of_threads){
-        actual_threads++;
-        bunch_of_threads.emplace_back([&](){quick_sort(tab, p+1, r);});
-    }
-    else{
-        quick_sort(tab, p+1, r);
-    }
+    quick_sort(tab, l, p-1);
+    quick_sort(tab, p+1, r);
 }
 
 int main(){
 
-    int n = 11;
+    int n = 100;
     int* tab = new int[n];
+    /*
     tab[0] = 3;
     tab[1] = 12;
     tab[2] = 45;
@@ -69,24 +51,41 @@ int main(){
     tab[8] = 5;
     tab[9] = 1;
     tab[10] = 7;
-
-    int number_of_threads = std::thread::hardware_concurrency();
-    std::cout<< "Number of max threads is: " << number_of_threads << "\n\n";
+    */
+    for (int i = 0; i < n; i++){
+        tab[i] = n-i;
+    }
 
     std::cout<< "\n";
     std::cout<< "Before sorting: ";
     print_tab(tab, n);
     std::cout<< "\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int i =  - 1;
+    int pivot = tab[n-1];
+
+    for (int j = 0; j <= n-2; j++){
+        if (tab[j] <= pivot){
+            i++;
+            std::swap(tab[i], tab[j]);
+        }
+    }
+
+    int p = i+1;
+    std::swap(tab[i+1], tab[n-1]);
+
+    std::async(std::launch::async, quick_sort, tab, 0, p-1);
+    std::async(std::launch::async, quick_sort, tab, p+1, n-1);
     
-    quick_sort(tab, 0, n-1); 
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout<< "Time of parallel: " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count() << "\n";
 
     std::cout<< "After sortng:   ";
     print_tab(tab, n);
     std::cout<<"\n";
 
-    for (auto&& thread : bunch_of_threads){
-        thread.detach();
-    }
-
+    delete[] tab;
     return 0;
 }
